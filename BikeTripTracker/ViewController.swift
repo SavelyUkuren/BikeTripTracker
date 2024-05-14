607377ea0cdb82f0180df607491e3009dbcf1bc7
Savely <125015568+SavelyUkuren@users.noreply.github.com>
2024-05-14 20:20:34 +0300

    Initial Commit

---
+++ BikeTripTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BikeTripTracker
+//
+//  Created by savik on 14.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
