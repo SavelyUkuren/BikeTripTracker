892d0e72341bd6a9611d9420a396ffe14ebfdcb7
Savely <125015568+SavelyUkuren@users.noreply.github.com>
2024-05-21 15:09:37 +0300

    Make Settings layout in storyboard

---
+++ BikeTripTracker/ViewControllers/Settings/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  BikeTripTracker
+//
+//  Created by savik on 21.05.2024.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
